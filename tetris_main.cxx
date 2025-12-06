99e030072bcd9b803ebbf66dffacbc9b750c3fa1
trung300906 <taduytrung123@gmail.com>
2025-12-06 20:11:22 +0700

    update code base

---
+++ tetris_main.cxx
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+#define H 20
+#define W 15
+char board[H][W] = {} ;
+char blocks[][4][4] = {
+        {{' ','I',' ',' '},
+         {' ','I',' ',' '},
+         {' ','I',' ',' '},
+         {' ','I',' ',' '}},
+        {{' ','I',' ',' '},
+         {' ','I',' ',' '},
+         {' ','I',' ',' '},
+         {' ','I',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ','O','O',' '},
+         {' ','O','O',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ','O','O',' '},
+         {' ','O','O',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ','O','O',' '},
+         {' ','O','O',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ','O','O',' '},
+         {' ','O','O',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ','O','O',' '},
+         {' ','O','O',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ','O','O',' '},
+         {' ','O','O',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ','O','O',' '},
+         {' ','O','O',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {'I','I','I','I'},
+         {' ',' ',' ',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ','O','O',' '},
+         {' ','O','O',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ','T',' ',' '},
+         {'T','T','T',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ','S','S',' '},
+         {'S','S',' ',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {'Z','Z',' ',' '},
+         {' ','Z','Z',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {'J',' ',' ',' '},
+         {'J','J','J',' '},
+         {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '},
+         {' ',' ','L',' '},
+         {'L','L','L',' '},
+         {' ',' ',' ',' '}}
+};
+
+int x=4,y=0,b=1;
+void gotoxy(int x, int y) {
+    COORD c = {x, y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+void boardDelBlock(){
+    for (int i = 0 ; i < 4 ; i++)
+        for (int j = 0 ; j < 4 ; j++)
+            if (blocks[b][i][j] != ' ' && y+j < H)
+                board[y+i][x+j] = ' ';
+}
+void block2Board(){
+    for (int i = 0 ; i < 4 ; i++)
+        for (int j = 0 ; j < 4 ; j++)
+            if (blocks[b][i][j] != ' ' )
+                board[y+i][x+j] = blocks[b][i][j];
+}
+void initBoard(){
+    for (int i = 0 ; i < H ; i++)
+        for (int j = 0 ; j < W ; j++)
+            if ((i==H-1) || (j==0) || (j == W-1)) board[i][j] = '#';
+            else board[i][j] = ' ';
+}
+void draw(){
+    gotoxy(0,0);
+    for (int i = 0 ; i < H ; i++, cout<<endl)
+        for (int j = 0 ; j < W ; j++)
+            cout<<board[i][j];
+}
+bool canMove(int dx, int dy){
+    for (int i = 0 ; i < 4 ; i++)
+        for (int j = 0 ; j < 4 ; j++)
+            if (blocks[b][i][j] != ' '){
+                int tx = x + j + dx;
+                int ty = y + i + dy;
+                if ( tx<1 || tx >= W-1 || ty >= H-1) return false;
+                if ( board[ty][tx] != ' ') return false;
+            }
+    return true;
+}
+
+int main()
+{
+    srand(time(0));
+    b = rand() % 7;
+    system("cls");
+    initBoard();
+    while (1){
+        boardDelBlock();
+        if (kbhit()){
+            char c = getch();
+            if (c=='a' && canMove(-1,0)) x--;
+            if (c=='d' && canMove(1,0) ) x++;
+            if (c=='x' && canMove(0,1))  y++;
+            if (c=='q') break;
+        }
+        if (canMove(0,1)) y++;
+        else {
+            block2Board();
+            removeLine();
+            x = 5; y = 0; b = rand() % 7;
+        }
+        block2Board();
+        draw();
+        _sleep(200);
+    }
+    return 0;
+}
