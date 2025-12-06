2d5f81ad9213613b9934b5060d462fdc256091d5
todicode <nguyentrangiabao7100@gmail.com>
2025-12-07 00:21:52 +0700

    Xử lí tốc độ rơi của khổi mỗi khi removeLine

--- tetris_main.cxx
+++ tetris_main.cxx
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <ctime>
 using namespace std;
-#define H 20
-#define W 15
-char board[H][W] = {} ;
+
+#define H 21
+#define W 16
+
+char board[H][W] = {};
+
 char blocks[][4][4] = {
-        {{' ','I',' ',' '},
+    {{' ','I',' ',' '},
          {' ','I',' ',' '},
          {' ','I',' ',' '},
          {' ','I',' ',' '}},
@@ -72,71 +76,134 @@ char blocks[][4][4] = {
          {' ',' ',' ',' '}}
 };
 
-int x=4,y=0,b=1;
+int x = 4, y = 0, b = 1;
+int gameSpeed = 200;
+
 void gotoxy(int x, int y) {
-    COORD c = {x, y};
+    COORD c = { x, y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
-void boardDelBlock(){
-    for (int i = 0 ; i < 4 ; i++)
-        for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' ' && y+j < H)
-                board[y+i][x+j] = ' ';
+
+void showCursor(bool show) {
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO     cursorInfo;
+    GetConsoleCursorInfo(out, &cursorInfo);
+    cursorInfo.bVisible = show;
+    SetConsoleCursorInfo(out, &cursorInfo);
 }
-void block2Board(){
-    for (int i = 0 ; i < 4 ; i++)
-        for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' ' )
-                board[y+i][x+j] = blocks[b][i][j];
+
+void boardDelBlock() {
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (blocks[b][i][j] != ' ' && y + i < H && x + j < W)
+                board[y + i][x + j] = ' ';
 }
-void initBoard(){
-    for (int i = 0 ; i < H ; i++)
-        for (int j = 0 ; j < W ; j++)
-            if ((i==H-1) || (j==0) || (j == W-1)) board[i][j] = '#';
+
+void block2Board() {
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (blocks[b][i][j] != ' ' && y + i < H && x + j < W)
+                board[y + i][x + j] = blocks[b][i][j];
+}
+
+void initBoard() {
+    for (int i = 0; i < H; i++)
+        for (int j = 0; j < W; j++)
+            if ((i == H - 1) || (j == 0) || (j == W - 1)) board[i][j] = '#';
             else board[i][j] = ' ';
 }
-void draw(){
-    gotoxy(0,0);
-    for (int i = 0 ; i < H ; i++, cout<<endl)
-        for (int j = 0 ; j < W ; j++)
-            cout<<board[i][j];
+
+void draw() {
+    gotoxy(0, 0);
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            cout << board[i][j];
+        }
+        cout << endl;
+    }
 }
-bool canMove(int dx, int dy){
-    for (int i = 0 ; i < 4 ; i++)
-        for (int j = 0 ; j < 4 ; j++)
-            if (blocks[b][i][j] != ' '){
+
+bool canMove(int dx, int dy) {
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (blocks[b][i][j] != ' ') {
                 int tx = x + j + dx;
                 int ty = y + i + dy;
-                if ( tx<1 || tx >= W-1 || ty >= H-1) return false;
-                if ( board[ty][tx] != ' ') return false;
+                if (tx < 1 || tx >= W - 1 || ty >= H - 1) return false;
+                if (board[ty][tx] != ' ') return false;
             }
     return true;
+}
+
+void updateSpeed() {
+    gameSpeed -= 15;
+    if (gameSpeed < 50) gameSpeed = 50;
+}
+
+void removeLine() {
+    int k = H - 2;
+    for (int i = H - 2; i > 0; i--) {
+        int count = 0;
+        for (int j = 1; j < W - 1; j++) {
+            if (board[i][j] != ' ') count++;
+            board[k][j] = board[i][j];
+        }
+        
+        if (count < W - 2) {
+            k--; 
+        } else {
+            updateSpeed(); 
+        }
+    }
 }
 
 int main()
 {
     srand(time(0));
+    showCursor(false);
     b = rand() % 7;
-    system("cls");
     initBoard();
-    while (1){
+    while (1) {
         boardDelBlock();
-        if (kbhit()){
-            char c = getch();
-            if (c=='a' && canMove(-1,0)) x--;
-            if (c=='d' && canMove(1,0) ) x++;
-            if (c=='x' && canMove(0,1))  y++;
-            if (c=='q') break;
+        if (_kbhit()) {
+            int c = _getch();
+            if (c == 0 || c == 224) {
+                char arrow = _getch();
+                switch (arrow) {
+                case 75:
+                    if (canMove(-1, 0)) x--;
+                    break;
+                case 77:
+                    if (canMove(1, 0)) x++;
+                    break;
+                case 80:
+                    if (canMove(0, 1)) y++;
+                    break;
+                case 72:
+                    break;
+                }
+            }
+            else {
+                if ((c == 'a' || c == 'A') && canMove(-1, 0)) x--;
+                if ((c == 'd' || c == 'D') && canMove(1, 0)) x++;
+                if ((c == 's' || c == 'S') && canMove(0, 1)) y++;
+                if (c == 'q' || c == 'Q') break;
+            }
         }
-        if (canMove(0,1)) y++;
+
+        if (canMove(0, 1)) 
+            y++;
         else {
             block2Board();
             removeLine();
-            x = 5; y = 0; b = rand() % 7;
+            x = W / 2 - 2;
+            y = 0;
+            b = rand() % 7;
+            if (!canMove(0,0)) break;
         }
         block2Board();
         draw();
-        _sleep(200);
+        Sleep(gameSpeed);
     }
     return 0;
 }
